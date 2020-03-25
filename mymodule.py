bfb5c4ede4b4b374ee37f2fb55ca86f46030f4a5
guanyouchen <62639069+guanyouchen@users.noreply.github.com>
2020-03-25 15:48:00 +0800

    Update mymodule.py
    Changed function name of get_weather() to get_weather_of().

--- mymodule.py
+++ mymodule.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
  
  api_key = "xxxxxxx"
  
- def get_weather(city):
+ def get_weather_of(city):
      sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" +
                             city + "&appid=" + api_key)
      result = sock.read()                            
